9dafff0784859caa032c53cc434269e69329fb40
Sylvain Bertrand <sbertrand@ihmc.us>
2019-03-10 19:02:46 -0500

    Stuff for debugging getClosestFace().

--- src/main/java/us/ihmc/euclid/visualizers/ConvexPolytope3DVisualizer.java
+++ src/main/java/us/ihmc/euclid/visualizers/ConvexPolytope3DVisualizer.java
@@ -21,14 +21,12 @@ import us.ihmc.euclid.axisAngle.AxisAngle;
 import us.ihmc.euclid.geometry.interfaces.Vertex3DSupplier;
 import us.ihmc.euclid.geometry.tools.EuclidGeometryTools;
 import us.ihmc.euclid.shape.convexPolytope.ConvexPolytope3D;
-import us.ihmc.euclid.shape.convexPolytope.interfaces.ConvexPolytope3DReadOnly;
 import us.ihmc.euclid.shape.convexPolytope.interfaces.Face3DReadOnly;
 import us.ihmc.euclid.shape.convexPolytope.interfaces.HalfEdge3DReadOnly;
 import us.ihmc.euclid.shape.convexPolytope.tools.ConvexPolytope3DTroublesomeDataset;
 import us.ihmc.euclid.tools.EuclidCoreRandomTools;
 import us.ihmc.euclid.tuple3D.Point3D;
 import us.ihmc.euclid.tuple3D.Vector3D;
-import us.ihmc.euclid.tuple3D.interfaces.Point3DReadOnly;
 import us.ihmc.euclid.tuple3D.interfaces.Tuple3DReadOnly;
 import us.ihmc.graphicsDescription.MeshDataGenerator;
 import us.ihmc.graphicsDescription.MeshDataHolder;
@@ -46,28 +44,31 @@ public class ConvexPolytope3DVisualizer extends Application
       FocusBasedCameraMouseEventHandler cameraController = view3dFactory.addCameraController(0.005, 100.0, true);
       cameraController.setMinLatitude(Double.NEGATIVE_INFINITY);
       cameraController.setMaxLatitude(Double.POSITIVE_INFINITY);
-      view3dFactory.addWorldCoordinateSystem(0.0001);
+      view3dFactory.addWorldCoordinateSystem(0.1);
       view3dFactory.addNodeToView(new AmbientLight(Color.GRAY));
       view3dFactory.addPointLight(-10.0, 0.0, -1.0, Color.WHEAT);
 
-      ConvexPolytope3DTroublesomeDataset dataset = new ConvexPolytope3DTroublesomeDataset_20190305_230259();
+      ConvexPolytope3DTroublesomeDataset dataset = new ConvexPolytope3DTroublesomeDataset_20190310_190049();
 
       ConvexPolytope3D convexPolytope3D = new ConvexPolytope3D(Vertex3DSupplier.asVertex3DSupplier(dataset.getPointsBeforeIssue()),
                                                                dataset.getConstructionEpsilon());
       //      convexPolytope3D.addVertex(dataset.getTroublesomePoint());
-      view3dFactory.addNodeToView(generateFace3DsMesh(convexPolytope3D.getFaces()));
-      view3dFactory.addNodeToView(generateFace3DsNormalMesh(convexPolytope3D.getFaces()));
-      view3dFactory.addNodeToView(generatePointMesh(dataset.getTroublesomePoint(), Color.AQUAMARINE, 0.1));
+      view3dFactory.addNodeToView(generatePointMesh(dataset.getTroublesomePoint(), Color.BLACK, 0.1));
       Vector3D supportDirection = new Vector3D();
       supportDirection.sub(dataset.getTroublesomePoint(), convexPolytope3D.getCentroid());
+      view3dFactory.addNodeToView(generatePointMesh(convexPolytope3D.getSupportingVertex(supportDirection), Color.YELLOWGREEN, 0.1));
+      view3dFactory.addNodeToView(generatePointMesh(convexPolytope3D.getCentroid(), Color.DARKVIOLET, 0.1));
+      view3dFactory.addNodeToView(generateFace3DsMesh(convexPolytope3D.getFaces()));
+      view3dFactory.addNodeToView(generateFace3DsNormalMesh(convexPolytope3D.getFaces()));
 
-//      view3dFactory.addNodeToView(generateFace3DsMesh(Collections.singletonList(convexPolytope3D.getClosestFace(dataset.getTroublesomePoint())),
-//                                                      Color.DARKRED));
-            view3dFactory.addNodeToView(generateFace3DsMesh(Collections.singletonList(findClosestFace(convexPolytope3D, dataset.getTroublesomePoint())),
-                                                            Color.GREEN));
+      Face3DReadOnly naiveClosestFace = convexPolytope3D.getFaces().stream().sorted((f1, f2) -> Double.compare(f1.distance(dataset.getTroublesomePoint()),
+                                                                                                               f2.distance(dataset.getTroublesomePoint())))
+                                                        .findFirst().get();
+      view3dFactory.addNodeToView(generateFace3DsMesh(Collections.singletonList(naiveClosestFace), Color.DARKRED));
+      //      view3dFactory.addNodeToView(generateFace3DsMesh(Collections.singletonList(convexPolytope3D.getClosestFace(dataset.getTroublesomePoint())), Color.GREEN));
 
-      System.out.println(findClosestFace(convexPolytope3D, dataset.getTroublesomePoint()).isPointDirectlyAboveOrBelow(dataset.getTroublesomePoint()));
-      System.out.println(convexPolytope3D.getClosestFace(dataset.getTroublesomePoint()).isPointDirectlyAboveOrBelow(dataset.getTroublesomePoint()));
+      //      System.out.println(naiveClosestFace.signedDistanceToPlane(dataset.getTroublesomePoint()));
+      //      System.out.println(convexPolytope3D.getClosestFace(dataset.getTroublesomePoint()).signedDistanceToPlane(dataset.getTroublesomePoint()));
 
       primaryStage.setTitle(getClass().getSimpleName());
       primaryStage.setMaximized(true);
@@ -99,7 +100,7 @@ public class ConvexPolytope3DVisualizer extends Application
          MeshView outsideFaceNode = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Polygon(ccwFaceVertices)));
          outsideFaceNode.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> System.out.println(face.toString()));
          double hue = EuclidCoreRandomTools.nextDouble(new Random(face.hashCode()), 0.0, 360.0);
-         outsideFaceNode.setMaterial(new PhongMaterial(Color.hsb(hue, 0.9, 0.9, 1.0)));
+         outsideFaceNode.setMaterial(new PhongMaterial(Color.hsb(hue, 0.9, 0.9, 0.7)));
          group.getChildren().add(outsideFaceNode);
 
          //         MeshView insideFaceNode = new MeshView(JavaFXMeshDataInterpreter.interpretMeshData(MeshDataGenerator.Polygon(cwFaceVertices)));
@@ -182,111 +183,40 @@ public class ConvexPolytope3DVisualizer extends Application
       return Color.hsb(EuclidCoreRandomTools.nextDouble(random, 0.0, 360.0), 0.9, 0.9);
    }
 
-   public static class ConvexPolytope3DTroublesomeDataset_20190305_230259 extends ConvexPolytope3DTroublesomeDataset
+   public class ConvexPolytope3DTroublesomeDataset_20190310_190049 extends ConvexPolytope3DTroublesomeDataset
    {
-      public ConvexPolytope3DTroublesomeDataset_20190305_230259()
+      public ConvexPolytope3DTroublesomeDataset_20190310_190049()
       {
-         pointsBeforeIssue.add(new Point3D(0.42916866109613850000, 0.35206728716550995000, 0.24198843507781653000));
-         pointsBeforeIssue.add(new Point3D(1.46433067655042670000, -5.26377783593307850000, 2.44618277677921370000));
-         pointsBeforeIssue.add(new Point3D(0.61830334684053940000, 2.66349260406482370000, 6.38060838183427600000));
-         pointsBeforeIssue.add(new Point3D(1.37824795980955270000, -4.98401625119844650000, 8.19666411831393300000));
-         pointsBeforeIssue.add(new Point3D(0.39619803279994414000, 0.44391838094703484000, 9.72098574072407100000));
-         pointsBeforeIssue.add(new Point3D(-0.26336361752563420000, -5.16126006270603450000, 8.86228850474557000000));
-         pointsBeforeIssue.add(new Point3D(-3.12070099267314500000, 2.90520685186159880000, 6.39474568510551040000));
-         pointsBeforeIssue.add(new Point3D(-3.48157599866199000000, 2.93922795294413050000, 1.71252352249696440000));
-         pointsBeforeIssue.add(new Point3D(-2.87951312292417330000, -6.90648497714716100000, 0.72833263983926160000));
-         pointsBeforeIssue.add(new Point3D(0.13861121914660401000, -6.52318469303109300000, 6.97138243677902400000));
-         pointsBeforeIssue.add(new Point3D(-6.51223087001668800000, 2.86354377464792700000, 4.77084171815430100000));
-         pointsBeforeIssue.add(new Point3D(-6.31679849968808800000, 0.95853464225646600000, 8.52267478973442200000));
-         pointsBeforeIssue.add(new Point3D(-7.64597685912712500000, -0.07575959233231178000, 6.32871693283292700000));
-         pointsBeforeIssue.add(new Point3D(-5.77449537389847800000, 0.91519549893172590000, 9.38387291036445000000));
-         pointsBeforeIssue.add(new Point3D(-1.93743382960031770000, 1.98238452138590930000, 8.32900725005948500000));
-         pointsBeforeIssue.add(new Point3D(-6.11840079677843700000, -2.09781287024978360000, 0.07049430718593896000));
-         pointsBeforeIssue.add(new Point3D(-7.75265488757376200000, -1.95895694857336100000, 0.54151360675960800000));
-         pointsBeforeIssue.add(new Point3D(-7.08717097972498960000, -4.89751394464854800000, 1.59825847267978200000));
-         pointsBeforeIssue.add(new Point3D(-8.27817599967944200000, -4.45189333962035000000, 5.86399813481871700000));
-         pointsBeforeIssue.add(new Point3D(-2.44177070225528150000, 2.75592679803204000000, 0.63511959090570350000));
-         pointsBeforeIssue.add(new Point3D(-8.32041956802220300000, -2.66176636678776560000, 5.61847328699008800000));
-         pointsBeforeIssue.add(new Point3D(-4.97112015016016600000, -0.97990274519680120000, 9.73185501783019500000));
-         pointsBeforeIssue.add(new Point3D(-6.39642541488284200000, -6.31532117691915100000, 8.92760802580350300000));
-         pointsBeforeIssue.add(new Point3D(0.73108401443885280000, -1.79992017788094930000, 0.83140647918804780000));
-         pointsBeforeIssue.add(new Point3D(-2.62537465739533450000, -4.87023023131946700000, 0.11512860469325048000));
-         pointsBeforeIssue.add(new Point3D(-6.41605375410656500000, -1.69013847222851420000, 8.66544643548335700000));
-         pointsBeforeIssue.add(new Point3D(-6.78540730466985400000, -5.84718005795835700000, 8.52618891230881100000));
-         pointsBeforeIssue.add(new Point3D(-6.47344344882216000000, -6.74681158021361000000, 8.87479129834585300000));
-         troublesomePoint.set(-7.06446074090651100000, -1.40670426625248900000, -1.87693321160715950000);
+         pointsBeforeIssue.add(new Point3D(5.50959228909476600000, -7.37482512292480000000, -4.10818520693522900000));
+         pointsBeforeIssue.add(new Point3D(3.26408212934210870000, -7.39707117672959000000, -1.22355245643036800000));
+         pointsBeforeIssue.add(new Point3D(7.53014890474895500000, -6.27335116777345600000, 0.17761833927299975000));
+         pointsBeforeIssue.add(new Point3D(7.00957333504974900000, -4.66027284035378300000, -8.73680095021806300000));
+         pointsBeforeIssue.add(new Point3D(8.14949598684792200000, -6.49543245326867800000, -5.37935993441628200000));
+         pointsBeforeIssue.add(new Point3D(8.35662907631226300000, 1.84543837022117600000, -5.39953823637316200000));
+         pointsBeforeIssue.add(new Point3D(2.96308939191737770000, 1.76142157519687900000, -8.77596291853211800000));
+         pointsBeforeIssue.add(new Point3D(-0.93580603815346650000, -4.92396725159303900000, -0.11532085278145665000));
+         pointsBeforeIssue.add(new Point3D(-0.76463657796814740000, 1.03347633175171080000, 0.80886057878909770000));
+         pointsBeforeIssue.add(new Point3D(-0.28129539962552474000, -3.53342168472010170000, 0.49480723626103007000));
+         pointsBeforeIssue.add(new Point3D(8.25634207586230100000, -6.37824459900945900000, -1.87629904675217580000));
+         pointsBeforeIssue.add(new Point3D(-1.42124933586592480000, -4.42321621569930000000, -8.93592189307561400000));
+         pointsBeforeIssue.add(new Point3D(-0.98563205874393670000, 1.80198735635120320000, -9.09616868123428900000));
+         pointsBeforeIssue.add(new Point3D(-1.57445104301922000000, -6.43311386664316500000, -5.34389229822706300000));
+         pointsBeforeIssue.add(new Point3D(-0.68926046477887400000, -7.56637055170942000000, -6.83078977271695800000));
+         pointsBeforeIssue.add(new Point3D(1.69274158458135160000, -7.09522813808260600000, -8.82078837159815900000));
+         pointsBeforeIssue.add(new Point3D(5.26469169713043900000, -7.39208674608942600000, -4.21398782657623300000));
+         pointsBeforeIssue.add(new Point3D(-1.36613299846150400000, -6.70484031508754900000, -2.02753288121639040000));
+         pointsBeforeIssue.add(new Point3D(6.76141818392682700000, -5.48910896683029000000, -9.05252572352598300000));
+         pointsBeforeIssue.add(new Point3D(-1.48478727745275300000, -5.48177742169217550000, -1.08538604047731720000));
+         pointsBeforeIssue.add(new Point3D(1.96062734221581360000, -1.94952655262267700000, -9.07837819535282400000));
+         pointsBeforeIssue.add(new Point3D(4.13367820793450100000, 2.38250239054842600000, 0.26226012065547977000));
+         pointsBeforeIssue.add(new Point3D(1.46656243159462420000, 2.35701339241588850000, -6.82990764153806750000));
+         pointsBeforeIssue.add(new Point3D(0.73320119057805310000, -7.00643772944042400000, -0.94710004057561110000));
+         pointsBeforeIssue.add(new Point3D(6.51576151610400000000, 1.45489109336699580000, -0.75991614441446490000));
+         pointsBeforeIssue.add(new Point3D(6.21350164476770850000, -0.81771773142268160000, 0.16804616890483004000));
+         pointsBeforeIssue.add(new Point3D(3.46824065760577300000, -3.92823742831175070000, 0.79287770829049190000));
+         pointsBeforeIssue.add(new Point3D(2.17126713572093170000, 2.34231187412905600000, -2.38050654586149500000));
+         troublesomePoint.set(6.28071978048610700000, -13.20013773779965400000, -5.21906497359370400000);
          constructionEpsilon = 1.0E-10;
-      }
-   }
-
-   public static Face3DReadOnly findClosestFace(ConvexPolytope3DReadOnly convexPolytope3D, Point3DReadOnly point)
-   {
-      boolean isOutside = false;
-      double maxNegativeDistance = Double.NEGATIVE_INFINITY;
-      Face3DReadOnly closestFace = null;
-
-      for (int faceIndex = 0; faceIndex < convexPolytope3D.getNumberOfFaces(); faceIndex++)
-      {
-         Face3DReadOnly face = convexPolytope3D.getFace(faceIndex);
-         double signedDistanceToPlane = face.signedDistanceToPlane(point);
-
-         if (signedDistanceToPlane < 0.0)
-         {
-            maxNegativeDistance = Math.max(maxNegativeDistance, signedDistanceToPlane);
-            closestFace = face;
-         }
-         else
-         {
-            isOutside = true;
-            closestFace = face;
-            break;
-         }
-      }
-
-      if (!isOutside)
-         return closestFace;
-
-      boolean goToNeighbor = false;
-      boolean isQueryDirectlyAboveFace = true;
-      Face3DReadOnly candidateFace = null;
-      double distanceToClosestEdge = Double.POSITIVE_INFINITY;
-
-      while (true)
-      {
-         goToNeighbor = false;
-         isQueryDirectlyAboveFace = true;
-         distanceToClosestEdge = Double.POSITIVE_INFINITY;
-
-         for (int edgeIndex = 0; edgeIndex < closestFace.getNumberOfEdges(); edgeIndex++)
-         {
-            HalfEdge3DReadOnly edge = closestFace.getEdge(edgeIndex);
-            boolean isEdgeVisible = closestFace.canObserverSeeEdge(point, edge);
-
-            if (isEdgeVisible)
-            { // Visit neighbor.
-               isQueryDirectlyAboveFace = false;
-               HalfEdge3DReadOnly neighborEdge = edge.getTwin();
-               candidateFace = neighborEdge.getFace();
-               boolean isNeighborEdgeVisible = candidateFace.canObserverSeeEdge(point, neighborEdge);
-
-               if (!isNeighborEdgeVisible)
-               {
-                  closestFace = candidateFace;
-                  goToNeighbor = true;
-                  break;
-               }
-               else
-               {
-                  distanceToClosestEdge = Math.min(distanceToClosestEdge, neighborEdge.distance(point));
-               }
-            }
-
-         }
-
-         if (!goToNeighbor)
-         {
-            return closestFace;
-         }
       }
    }
 
