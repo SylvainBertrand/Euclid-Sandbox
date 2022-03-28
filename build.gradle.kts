fdcad687169802e6c8d6bd8b6740c82e2b067878
Valkyrie <valkyrie@nasa.gov>
2022-03-28 11:50:36 -0500

    Updated gradle scripts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-   id("us.ihmc.ihmc-build") version "0.20.1"
-   id("us.ihmc.ihmc-ci") version "5.3"
+   id("us.ihmc.ihmc-build")
 }
 
 ihmc {
@@ -15,7 +14,7 @@ ihmc {
 
 mainDependencies {
     api("org.ejml:dense64:0.30")
-    api("us.ihmc:ihmc-javafx-toolkit:0.12.7")
-    api("us.ihmc:euclid-shape:0.14.0")
-    api("us.ihmc:euclid-test:0.14.0")
+    api("us.ihmc:ihmc-javafx-toolkit:0.20.0")
+    api("us.ihmc:euclid-shape:0.17.2")
+    api("us.ihmc:euclid-test:0.17.2")
 }
